fcb9ba84b134bda76bb3b865333dc770f7964b04
gowtham-star <gowthamtnvs@yahoo.com>
2023-09-06 18:14:39 -0400

    Pushing 1st version of changes

---
+++ android/app/src/main/java/com/example/data_transfer_bluetooth/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.data_transfer_bluetooth;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BluetoothApp(),
+    );
+  }
+}
+
+class BluetoothApp extends StatefulWidget {
+  @override
+  _BluetoothAppState createState() => _BluetoothAppState();
+}
+
+class _BluetoothAppState extends State<BluetoothApp> {
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+  BluetoothDevice? raspberryPi;
+
+  void _connectToRaspberryPi() async {
+    // Start scanning for nearby Bluetooth devices
+    flutterBlue.scan().listen((scanResult) {
+      if (scanResult.device.name == 'Your Raspberry Pi Name') {
+        // Found the Raspberry Pi, stop scanning
+        flutterBlue.stopScan();
+        setState(() {
+          raspberryPi = scanResult.device;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bluetooth App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: _connectToRaspberryPi,
+              child: Text('Connect to Raspberry Pi'),
+            ),
+            Text('Connected to: ${raspberryPi?.name ?? 'None'}'),
+          ],
+        ),
+      ),
+    );
+  }
+}
